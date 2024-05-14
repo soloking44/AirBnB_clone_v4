680064f2e1a3b5314b8a997551d4699cd313d41c
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-05-14 22:05:17 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,10 +19,9 @@ app.url_map.strict_slashes = False
 
 # flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5001)
+port = os.getenv('HBNB_API_PORT', 5000)
 
 # Cross-Origin Resource Sharing
-# cors = CORS(app, resources={r'/*': {'origins': host}})
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 # app_views BluePrint defined in api.v1.views
