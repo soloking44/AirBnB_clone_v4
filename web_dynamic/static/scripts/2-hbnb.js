29e8933d3785c8fdc07ced01216b00e03d755531
soloking44 <solop8483@gmail.com>
2024-05-15 08:56:45 -0700

    2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,38 +1,25 @@
-$(document).ready(function () {
-  const amenities = {};
-  $('input[type="checkbox"]').click(function () {
-    const amenityId = $(this).attr('data-id');
-    const amenityName = $(this).attr('data-name');
-    if ($(this).prop('checked') === true) {
-      amenities[amenityId] = amenityName;
-    } else if ($(this).prop('checked') === false) {
-      delete amenities[amenityId];
-    }
-    const amenityList = Object.values(amenities).join(', ');
-    if (amenityList.length > 30) {
-      $('.amenities h4').text(amenityList.substring(0, 29) + '...');
+window.addEventListener('load', function () {
+  // task 3
+  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
     } else {
-      $('.amenities h4').text(amenityList);
-    }
-    if ($.isEmptyObject(amenities)) {
-      $('.amenities h4').html('&nbsp;');
+      $('#api_status').removeClass('available');
     }
   });
-  $.ajax(
-    {
-     url: 'http://0.0.0.0:5001/api/v1/status/',
-     type: 'GET',
-     dataType: 'json',
-     success: function (response) {
-	 if (response.status === 'OK') {
-	     $('DIV#api_status').addClass('available');
-	 } else {
-	     $('DIV#api_status').removeClass('available');
-	 }
-    },
-	error: function (error) {
-	    $('DIV#api_status').removeClass('available');
-	}
+
+  // task 2
+  const amenityIds = {};
+  $('input[type=checkbox]').click(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
     }
-  )
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
+  });
 });
